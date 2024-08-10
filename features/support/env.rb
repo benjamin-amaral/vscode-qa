3bb633aee39ec61de65136860400a3532422cc4d
Benjamin Amaral <68580342+benjamin-amaral@users.noreply.github.com>
2024-08-09 22:35:40 -0400

    Update linkedin to google

--- features/support/env.rb
+++ features/support/env.rb
@@ -7,6 +7,6 @@ require 'rspec/expectations'
 
 Capybara.configure do |config|
     config.default_driver = :selenium_chrome
-    config.app_host = 'http://www.linkedin.com'
+    config.app_host = 'http://www.google.com'
     config.default_max_wait_time = 5
 end
