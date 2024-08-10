4a467985a46c81371f5c1606a01f63fc7677d127
Benjamin Amaral <68580342+benjamin-amaral@users.noreply.github.com>
2024-08-10 00:46:16 -0400

    Escrevendo BDD

--- features/step_definitions/login.step.rb
+++ features/step_definitions/login.step.rb
@@ -1,12 +1,12 @@
-Dado('que loguei') do
-   visit ''
-   sleep 5
-  end
-  
-  Quando('eu logar') do
-    
-  end
-  
-  Entao('logaremos') do
-    
-  end
+Dado('que o usuario queira se logar') do
+   visit''
+   sleep 5   
+ end
+ 
+ Quando('ele digitar as credenciais validas') do
+   
+ end
+ 
+ Entao('deve acessar o site com sucesso') do
+   
+ end
